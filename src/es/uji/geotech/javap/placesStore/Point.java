a04a33748c8ed7d69977931aa662bf2744e47d4b
Bhuvangsr <gumuhb@gmail.com>
2013-11-21 09:48:37 +0100

    Package Structure Change

---
+++ src/es/uji/geotech/javap/placesStore/Point.java
@@ -0,0 +1,26 @@
+package es.uji.geotech.javap.placesStore;
+
+public class Point extends Artifact {
+
+	private Coordinates coordinates;
+	
+	public Point(){
+		
+	}
+	
+	
+	private void setCoords(){
+		
+	}
+	
+	public String getCoords(){
+		return "Hola!!";
+	}
+	
+	public String getAsKML(){
+		return "Hola!!";
+	}
+	
+	
+	
+}
